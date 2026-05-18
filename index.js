25d00c316889d6058fb5e08372bbe2b0d089efee
Valarie A. Paiz <paiz.valerie27@gmail.com>
2026-05-18 02:29:57 -0500

    "Practicing generating random numbers using a rolling die"

--- index.js
+++ index.js
@@ -224,6 +224,32 @@
 
 // RANDOM NUMBER GENERATOR!!!~~~~
 
-let randomNum = Math.floor(Math.random() * 6); // returns a random number between 0 (inclusive) and 6 (exclusive)
+// const min = 50;
+// const max = 100;
 
-console.log(randomNum);
+// let randomNum = Math.floor(Math.random() * (max - min)) + min; // returns a random number between 1 (inclusive) and 100 (inclusive)
+
+// console.log(randomNum);
+
+const myButton = document.getElementById("myButton");
+const myLabel = document.getElementById("myLabel");
+const myLabel1 = document.getElementById("myLabel1");
+const myLabel2 = document.getElementById("myLabel2");
+const myLabel3 = document.getElementById("myLabel3");
+
+const min = 1;
+const max = 6;
+let randomNum1;
+let randomNum2;
+let randomNum3;
+
+myButton.onclick = function(){
+    randomNum1 = Math.floor(Math.random() * max) + min;
+    randomNum2 = Math.floor(Math.random() * max) + min;
+    randomNum3 = Math.floor(Math.random() * max) + min;
+    myLabel1.textContent = randomNum1;
+    myLabel2.textContent = randomNum2;
+    myLabel3.textContent = randomNum3;
+    
+    myLabel.textContent = `You rolled a ${randomNum}!`;
+}
